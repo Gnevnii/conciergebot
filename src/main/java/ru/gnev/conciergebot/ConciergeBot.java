1a7b0c1232645e5c46f68bf6f49e03dfc9c787e5
taneev_ar <taneev_ar@interrao.ru>
2022-05-27 01:07:29 +0300

    CTB-0002 Добавление бота

---
+++ src/main/java/ru/gnev/conciergebot/ConciergeBot.java
@@ -0,0 +1,31 @@
+package ru.gnev.conciergebot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class ConciergeBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(final Update update) {
+
+    }
+}
