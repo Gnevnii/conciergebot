04973cae7d06842e3cabb7e3c9a2ae844af11317
gnev <gnev@yandex.ru>
2022-07-21 00:58:57 +0300

    Revert "описание работы бота. changelog"
    This reverts commit d6f6dacb62d45cd82be4788b233253e5f584b203.

--- src/main/resources/db/changelog/changesets/00003_add_column.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
-
--- changeset gnev:1
-alter table chat add column i_floor_count int, add column i_section_count int;
-comment on column chat.i_floor_count is 'Количество этажей';
-comment on column chat.i_section_count is 'Количество планов на этаже';
